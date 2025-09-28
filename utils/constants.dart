f016a726c1f8c254805b70300588cc76a1d97127
Justin <errolpriniel@gmail.com>
2025-09-28 23:03:35 +0800

    Update constants.dart

--- utils/constants.dart
+++ utils/constants.dart
@@ -1,1 +1,13 @@
+import 'package:flutter/material.dart';
 
+// --- UI Design Constants ---
+const Color kDarkScaffoldBackgroundColor = Color(0xFF0A1931);
+const Color kDarkCardBackgroundColor = Color(0xFF183A5A);
+const Color kLightScaffoldBackgroundColor = Color(0xFFF4F6FD);
+const Color kLightCardBackgroundColor = Colors.white;
+const Color kPrimaryTextColorDark = Colors.white;
+const Color kPrimaryTextColorLight = Color(0xFF0A1931);
+const Color kSecondaryTextColor = Color(0xFF9E9E9E);
+const Color kAccentColor = Color(0xFF3B72FF);
+const Color kChartLineColor = Color(0xFF33D69F);
+const Color kWarningColor = Color(0xFFFFA000);
